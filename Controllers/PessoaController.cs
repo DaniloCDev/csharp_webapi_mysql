ae374f09a6d52357d60c2d6e4c40ec667eaeff8c
ItKarma <loppesofc@gmail.com>
2025-01-19 13:35:44 -0300

    feat: add controller with model person

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Person.Model;
 
 namespace Person.Controller
 {
@@ -11,6 +12,13 @@ namespace Person.Controller
         public string MyFirstRoute()
         {
             return "Hello world";
+        }
+
+
+        [HttpPost]
+        public PersonModel ManipularModelPerson([FromBody] PersonModel p)
+        {
+            return p;
         }
     }
 }
