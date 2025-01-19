d666c80be9dea25a126692067fae4c9be1dcf1a8
ItKarma <loppesofc@gmail.com>
2025-01-19 13:36:05 -0300

    feat: add model person

---
+++ Models/Person.cs
@@ -0,0 +1,10 @@
+namespace Person.Model
+{
+    public class PersonModel
+    {
+        public int Codigo { get; set; }
+        public string? Nome { get; set; }
+        public string? Cidade { get; set; }
+        public int Idade { get; set; }
+    }
+}
